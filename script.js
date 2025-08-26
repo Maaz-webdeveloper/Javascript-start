2411d244b3492e7b053412a7c3a6f952cc2db8ee
Muhammad Maaz <maazrajpoot991@gmail.com>
2025-08-26 08:35:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+// Arithmetic operator tb use krta hain jb variable ki data type numbers ma ho.
+
+//Addition
+var a=4;
+var b=6;               
+var c=a+b;
+console.log(c);//print 10
+
+//Subtraction
+var a=2;
+var b=3;
+var c=a-b;
+console.log(c);//print -1
+
+//Multiplication
+var a=20;
+var b=100;
+var c=a*b;
+console.log(c);//print 2000
+
+//Division
+var a=20;
+var b=2;
+var c=a/b;
+console.log(c);//print 10
+
+//Modulus(Remainder ata ha is ma).
+var x=14;
+var y=2;
+var z=x%y;
+console.log(z);//print 0
+
+//Exponentiation[is ma 10 ki power 10 ho jai gi]
+var x=10;
+var y=2;
+var z=x**y;
+console.log(z);//print 100
+
+//Increament{is ma ak raqam bad jai gi}
+var a=13;
+var b=2;
+a++;
+console.log(a+b);//print 16
+
+//Decreament [is ma ak raqam km ho jai gi]
+var a=10;
+var b=2;
+a--;
+console.log(a+b);//print 11
+
+
+var a=6;
+var b=2;
+console.log(a+b);
+console.log("<br>");
+a--;
+console.log(a+b);//print 7
+
+
+var a=12;
+var b=2;
+console.log(a+b);
+console.log("<br>");
+a++;
+console.log(a+b);//print 15
+
+var x=5;
+var y=5;
+console.log(x==y);
+
+//Comparison operator
+
+var a=5;
+var b=5;
+console.log("5==5", a==b); 
+//print true
+
+var x=6;
+var y=5;
+console.log("6!=5", x!=y);
+//print true
+
+var a=5;//number
+var b="5";//string number ma convert ho ga is lia output true ay gi
+console.log("a===b",a===b);//print true
+
+var a=5;
+var b="5";
+console.log("a!==b",a!==b);
+
+var x=5;
+var y=3;
+console.log("5>3",x>y);
+//print true
+
+var a=5;
+var b=3;
+console.log("5<3",a<b)
+
+
